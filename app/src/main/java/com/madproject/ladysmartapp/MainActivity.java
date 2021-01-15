d9ef216eebcc81bdf914f1501702ce8a7e87c231
vihara1124 <thilakshivihara1998@gmail.com>
2021-01-16 01:18:17 +0530

    tatvthilakarathne

--- app/src/main/java/com/madproject/ladysmartapp/MainActivity.java
+++ app/src/main/java/com/madproject/ladysmartapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.madproject.ladysmartapp;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,RegLogChoice.class));
+            }
+        },3000);
     }
 }
