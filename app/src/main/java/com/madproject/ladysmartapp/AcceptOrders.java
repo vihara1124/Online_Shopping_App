f9d080427019fa9abcc090081f4ae2fff3fbbf53
kaushalyahansini <kaushalya3hansini@gmail.com>
2021-01-17 23:37:50 +0530

    Update Application

---
+++ app/src/main/java/com/madproject/ladysmartapp/AcceptOrders.java
@@ -0,0 +1,14 @@
+package com.madproject.ladysmartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcceptOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept_orders);
+    }
+}
