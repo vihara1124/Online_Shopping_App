74772f0b7475d4a498015378be4b15feaefe1066
vihara1124 <thilakshivihara1998@gmail.com>
2021-01-19 23:23:28 +0530

    Update Application

---
+++ app/src/main/java/com/madproject/ladysmartapp/StaffLogin.java
@@ -0,0 +1,14 @@
+package com.madproject.ladysmartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_login);
+    }
+}
