760e54fbaf56249c338511c13ae11453bce25768
tharikawickramanayake <tharikawickramanayake@outlook.com>
2021-01-20 01:50:18 +0530

    Update Application

---
+++ app/src/main/java/com/madproject/ladysmartapp/Productdetails.java
@@ -0,0 +1,13 @@
+package com.madproject.ladysmartapp;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Productdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetails);
+    }
+}
