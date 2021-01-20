62f32c49027c1e345f9d186da55d16a9cc122ebe
vihara1124 <thilakshivihara1998@gmail.com>
2021-01-21 00:54:40 +0530

    Update Application

--- app/src/main/java/com/madproject/ladysmartapp/ToolsViewModel.java
+++ app/src/main/java/com/madproject/ladysmartapp/ToolsViewModel.java
@@ -3,11 +3,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class ToolsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public ToolsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("");
     }
