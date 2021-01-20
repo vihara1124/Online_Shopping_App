c9ce2efa28e6e36d0c4cc97189cf7ce278c526a6
vihara1124 <thilakshivihara1998@gmail.com>
2021-01-21 00:34:06 +0530

    Update Application

---
+++ app/src/main/java/com/madproject/ladysmartapp/SendViewModel.java
@@ -0,0 +1,18 @@
+package com.madproject.ladysmartapp;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SendViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SendViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
